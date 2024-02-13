b77c76affd6fdfdc750705a2488adeb83d0ccb00
venpy <norbedo@proton.me>
2024-02-13 12:11:36 +0100

    First commit

---
+++ services/webapp/code/app.py
@@ -0,0 +1,97 @@
+import os
+import uuid
+import logging
+import requests
+
+from flask import Flask, render_template, request
+from utils import os_shell
+
+# Set up logging
+logging.basicConfig(format="[%(levelname)s] %(asctime)s %(message)s", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def menu():
+    # Render opening menu page
+    return render_template("menu.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload_file_and_exam():
+
+    # Generate a grading session uuid
+    uuid_str = str(uuid.uuid4())
+        
+    # Set the grading directory folder with the grading session uuid
+    destination_folder = "/shared/{}".format(uuid_str)
+
+    # Set destination dir
+    try:
+        os.makedirs(destination_folder)
+    except PermissionError:
+        os_shell("sudo chmod 777 /shared")
+        os.makedirs(destination_folder)
+
+    # Set the destination exam file
+    exam_file = destination_folder + "/esame.py"
+    
+    # Get and save uploaded exam file or from URL
+    try:
+        url = request.form["url"]
+        if not url:
+            raise KeyError
+        logger.debug('Getting file from "{}"'.format(url))
+        r = requests.get(url, allow_redirects=True)
+        with open(exam_file, 'wb') as f:
+            f.write(r.content)
+    except KeyError:
+        f = request.files["file"]
+        f.save(exam_file)
+        logger.debug('Uploaded file')
+
+    
+    logger.debug("Saved exam file to '%s'", exam_file)
+
+    # Set exam tests file
+    exam_tests_file = "test_esame.py"
+    logger.debug("Set exam tests file to '%s'", exam_tests_file)
+
+    # Save exam tests file in the grading directory as well
+    source_exam_file = "/opt/webapp/code/tests/" + exam_tests_file
+    os_shell(" ".join(["cp", source_exam_file, destination_folder]))
+    logger.debug("Copied tests file to '%s'", source_exam_file)
+
+    # Grade and return
+    return grade(uuid_str)
+
+
+def grade(uuid_str):
+    
+    # Initialize page data
+    data = {}
+    
+    # Create the docker run command.
+    # To restrict RAM: --memory=\"256m\"
+    command = "sudo docker run --cpus=\"0.1\" -v /tmp/autograding_shared/:/shared autograding/evaluator /bin/bash -c 'cd /shared/" + uuid_str + " && python3 -m unittest discover'"
+    
+    # Log the shell command going to be execute
+    logger.debug("Shell executing command: \"%s\"", command)
+    
+    # Execute the command and capture the output
+    out = os_shell(command, capture=True)
+    
+    # Log the output
+    logger.debug("Shell output: \"%s\"", out)
+    
+    # Set the output in the page data
+    data["shell_out"] = out
+
+    # Render
+    return render_template("grade.html", data=data)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
---
+++ services/webapp/code/tests/test_esame.py
@@ -0,0 +1,315 @@
+import unittest
+from esame import CSVTimeSeriesFile, compute_increments, ExamException
+
+score = 0
+
+class TestComputeIncrements(unittest.TestCase):
+    
+    def test_get_data(self):
+        file = CSVTimeSeriesFile('/data/data.csv')
+        data = file.get_data()
+        self.assertEqual(len(data), 3) 
+        self.assertEqual(data[0], ['1949-01', 90])
+        self.assertEqual(data[-1], ['1950-11', 100])
+        
+        global score
+        score += 4
+    
+    def test_get_data_missing_file(self):
+        with self.assertRaises(ExamException):
+            CSVTimeSeriesFile('/data/missing.csv')
+        
+        global score
+        score += 1
+        
+    def test_get_data_empty_file(self):
+        with self.assertRaises(ExamException):
+            CSVTimeSeriesFile('/data/empty.csv')
+        
+        global score
+        score += 1
+        
+    def test_get_data_incorrect_header(self):
+        file = CSVTimeSeriesFile('/data/incorrect_header.csv')
+        with self.assertRaises(ExamException):
+            file.get_data()
+        
+        global score
+        score += 1
+    
+    def test_get_data_invalid_rows(self):
+        file = CSVTimeSeriesFile('/data/invalid_rows.csv')
+        with self.assertRaises(ExamException):
+            file.get_data()
+            
+        global score
+        score += 1
+        
+    def test_get_data_out_of_order(self):
+        file = CSVTimeSeriesFile('/data/dates_out_of_order.csv')
+        with self.assertRaises(ExamException):
+            file.get_data()
+            
+        global score
+        score += 1
+        
+    def test_get_data_dates_incorrect_format(self):
+        file = CSVTimeSeriesFile('/data/dates_incorrect_format.csv')
+        with self.assertRaises(ExamException):
+            file.get_data()
+            
+        global score
+        score += 1
+        
+    def test_get_data_months_range(self):
+        file = CSVTimeSeriesFile('/data/months_range.csv')
+        with self.assertRaises(ExamException):
+            file.get_data()
+            
+        global score
+        score += 1
+        
+    def test_get_data_duplicate_dates(self):
+        file = CSVTimeSeriesFile('/data/duplicate_dates.csv')
+        with self.assertRaises(ExamException):
+            file.get_data()
+            
+        global score
+        score += 1
+    
+    def test_get_data_months_out_of_order(self):
+        file = CSVTimeSeriesFile('/data/months_out_of_order.csv')
+        with self.assertRaises(ExamException):
+            file.get_data()
+            
+        global score
+        score += 1
+        
+    def test_get_data_passengers_string(self):
+        file = CSVTimeSeriesFile('/data/passengers_string.csv')
+        data = file.get_data()
+        self.assertEqual( len(data), 11)
+            
+        global score
+        score += 1
+        
+    def test_get_data_passengers_float(self):
+        file = CSVTimeSeriesFile('/data/passengers_float.csv')
+        data = file.get_data()
+        self.assertEqual( len(data), 11)
+            
+        global score
+        score += 1
+    
+    def test_get_data_passengers_negative(self):
+        file = CSVTimeSeriesFile('/data/passengers_negative.csv')
+        data = file.get_data()
+        self.assertEqual( len(data), 11)
+            
+        global score
+        score += 1
+    
+    def test_get_data_passengers_zero(self):
+        file = CSVTimeSeriesFile('/data/passengers_zero.csv')
+        data = file.get_data()
+        self.assertEqual( len(data), 11)
+            
+        global score
+        score += 1
+
+    def test_normal(self):
+        time_series = [['2020-01', 100], ['2020-02', 120], ['2021-01', 130], ['2021-02', 140]]
+        first_year = '2020' 
+        last_year = '2021'
+        expected = {'2020-2021': 25.0}
+        actual = compute_increments(time_series, first_year, last_year)
+        self.assertEqual(expected, actual)
+        
+        global score
+        score += 4
+        
+    def test_fist_end_four_digits(self):
+        time_series = [['2020-01', 100], ['2020-02', 120], ['2021-01', 130], ['2021-02', 140]]
+        first_year = '2020' 
+        last_year = '921'
+        with self.assertRaises(ExamException):
+            compute_increments(time_series, first_year, last_year)
+        
+        global score
+        score += 1
+        
+    def test_firs_end_not_digits(self):
+        time_series = [['2020-01', 100], ['2020-02', 120], ['2021-01', 130], ['2021-02', 140]]
+        first_year = '2020' 
+        last_year = 'test'
+        with self.assertRaises(ExamException):
+            compute_increments(time_series, first_year, last_year)
+        
+        global score
+        score += 1
+
+    def test_missing_year(self):
+        time_series = [['2020-01', 100], ['2020-02', 120], ['2022-01', 140], ['2022-02', 150]] 
+        first_year = '2020'
+        last_year = '2022'
+        expected = {'2020-2022': 35.0}
+        actual = compute_increments(time_series, first_year, last_year)
+        self.assertEqual(expected, actual)
+        
+        global score
+        score += 1
+
+    def test_empty_result(self):
+        time_series = [['2020-01', 100], ['2022-01', 120]]
+        first_year = '2020'
+        last_year = '2021' 
+        expected = {}
+        actual = compute_increments(time_series, first_year, last_year)
+        self.assertEqual(expected, actual)
+        
+        global score
+        score += 1
+
+    def test_invalid_years(self):
+        time_series = [['2020-01', 100], ['2020-02', 120]]
+        first_year = '202'
+        last_year = '2021'
+        with self.assertRaises(ExamException):
+            compute_increments(time_series, first_year, last_year)
+        
+        global score
+        score += 1
+
+    def test_years_not_in_order(self):
+        time_series = [['2020-01', 100], ['2021-02', 120]]
+        first_year = '2021'
+        last_year = '2020'
+        with self.assertRaises(ExamException):
+            compute_increments(time_series, first_year, last_year)
+
+        global score
+        score += 1
+
+    def test_years_not_in_data(self):
+        time_series = [['2020-01', 100], ['2020-02', 120]]
+        first_year = '2018'
+        last_year = '2020'
+        with self.assertRaises(ExamException):
+            compute_increments(time_series, first_year, last_year)
+            
+        global score
+        score += 1
+        
+    def test_missing_first_year(self):
+        time_series = [['2020-01', 100], ['2020-02', 120], ['2021-01', 130], ['2021-02', 140]]
+        first_year = '2019'
+        last_year = '2021'
+        with self.assertRaises(ExamException):
+            compute_increments(time_series, first_year, last_year)
+
+        global score
+        score += 1
+    
+    def test_holes(self):
+        time_series = [['2020-01', 100], ['2022-02', 120], ['2024-01', 130], ['2026-02', 140]]
+        first_year = '2020'
+        last_year = '2026'
+        expected = {'2020-2022': 20.0, '2022-2024': 10.0, '2024-2026': 10.0}
+        actual = compute_increments(time_series, first_year, last_year)
+        self.assertEqual(expected, actual)
+        
+        global score
+        score += 1
+
+    @classmethod
+    def tearDownClass(cls):
+        global score
+        print('\n\n-------------')
+        print('| Voto: {}  |'.format(score))
+        print('-------------\n')
+
+
+# Run the tests
+if __name__ == "__main__":
+    unittest.main()
+
+
+#===================================
+#  OS shell utility
+#===================================
+
+import subprocess
+from collections import namedtuple
+
+def sanitize_shell_encoding(text):
+    return text.encode("utf-8", errors="ignore").decode("utf-8")
+
+
+def format_shell_error(stdout, stderr, exit_code):
+    
+    string  = '\n#---------------------------------'
+    string += '\n# Shell exited with exit code {}'.format(exit_code)
+    string += '\n#---------------------------------\n'
+    string += '\nStandard output: "'
+    string += sanitize_shell_encoding(stdout)
+    string += '"\n\nStandard error: "'
+    string += sanitize_shell_encoding(stderr) +'"\n\n'
+    string += '#---------------------------------\n'
+    string += '# End Shell output\n'
+    string += '#---------------------------------\n'
+
+    return string
+
+
+def os_shell(command, capture=False, verbose=False, interactive=False, silent=False):
+    '''Execute a command in the OS shell. By default prints everything. If the capture switch is set,
+    then it returns a namedtuple with stdout, stderr, and exit code.'''
+    
+    if capture and verbose:
+        raise Exception('You cannot ask at the same time for capture and verbose, sorry')
+
+    # Log command
+    #logger.debug('Shell executing command: "%s"', command)
+
+    # Execute command in interactive mode    
+    if verbose or interactive:
+        exit_code = subprocess.call(command, shell=True)
+        if exit_code == 0:
+            return True
+        else:
+            return False
+
+    # Execute command getting stdout and stderr
+    # http://www.saltycrane.com/blog/2008/09/how-get-stdout-and-stderr-using-python-subprocess-module/
+    
+    process          = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    (stdout, stderr) = process.communicate()
+    exit_code        = process.wait()
+
+    # Convert to str (Python 3)
+    stdout = stdout.decode(encoding='UTF-8')
+    stderr = stderr.decode(encoding='UTF-8')
+
+    # Formatting..
+    stdout = stdout[:-1] if (stdout and stdout[-1] == '\n') else stdout
+    stderr = stderr[:-1] if (stderr and stderr[-1] == '\n') else stderr
+
+    # Output namedtuple
+    Output = namedtuple('Output', 'stdout stderr exit_code')
+
+    if exit_code != 0:
+        if capture:
+            return Output(stdout, stderr, exit_code)
+        else:
+            print(format_shell_error(stdout, stderr, exit_code))      
+            return False    
+    else:
+        if capture:
+            return Output(stdout, stderr, exit_code)
+        elif not silent:
+            # Just print stdout and stderr cleanly
+            print(stdout)
+            print(stderr)
+            return True
+        else:
+            return True
